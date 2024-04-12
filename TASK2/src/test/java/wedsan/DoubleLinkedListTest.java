982c0e32f863d3e1bd17e5d97ff6ec26ad446fb4
André <120421201+WedSan@users.noreply.github.com>
2024-04-11 22:14:02 -0300

    test: add test for removeMiddleElement method

--- TASK2/src/test/java/wedsan/DoubleLinkedListTest.java
+++ TASK2/src/test/java/wedsan/DoubleLinkedListTest.java
@@ -1,8 +1,8 @@
+package wedsan;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import wedsan.DoubleLinkedList;
-import wedsan.Node;
 import wedsan.Exception.MiddleElementNotFoundException;
 
 public class DoubleLinkedListTest {
@@ -22,6 +22,20 @@ public class DoubleLinkedListTest {
         doubleLinkedList.addElement(node2);
         Assertions.assertEquals(node1, doubleLinkedList.getFirstElement());
         Assertions.assertEquals(node2, doubleLinkedList.getLastElement());
+        Assertions.assertEquals(2, doubleLinkedList.getSize());
+    }
+
+    @Test
+    public void testRemoveMiddleElement() throws MiddleElementNotFoundException {
+        Node node1 = new Node("batman");
+        Node node2 = new Node("joker");
+        Node node3 = new Node("penguin");
+        doubleLinkedList.addElement(node1);
+        doubleLinkedList.addElement(node2);
+        doubleLinkedList.addElement(node3);
+        doubleLinkedList.removeMiddleElement();
+        Assertions.assertEquals(node1, doubleLinkedList.getFirstElement());
+        Assertions.assertEquals(node3, doubleLinkedList.getLastElement());
         Assertions.assertEquals(2, doubleLinkedList.getSize());
     }
 
