dd03fbbd8d99aa5521053c9327f322e106640638
André <120421201+WedSan@users.noreply.github.com>
2024-04-12 01:12:10 -0300

    feat: add the task implementation to main class

--- TASK2/src/main/java/wedsan/Main.java
+++ TASK2/src/main/java/wedsan/Main.java
@@ -1,7 +1,31 @@
 package wedsan;
 
+import wedsan.Exception.MiddleElementNotFoundException;
+
+import java.security.spec.RSAOtherPrimeInfo;
+
 public class Main {
     public static void main(String[] args) {
+
+        Node node1 = new Node("batman");
+        Node node2 = new Node("joker");
+        Node node3 = new Node("penguin");
+
+        DoubleLinkedList doubleLinkedList = new DoubleLinkedList();
+
+        doubleLinkedList.addElement(node1);
+        doubleLinkedList.addElement(node2);
+        doubleLinkedList.addElement(node3);
+        System.out.println("-- List before remove the middle element");
+        doubleLinkedList.printElements();
+
+        try {
+            doubleLinkedList.removeMiddleElement();
+        } catch (MiddleElementNotFoundException ex) {
+            System.out.println(ex.getMessage());
+        }
+        System.out.println("-- List after removing middle element --");
+        doubleLinkedList.printElements();
 
 
 
