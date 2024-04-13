a14ef1fb69a40e64693b9ddc77e4236556ccd2c4
André <120421201+WedSan@users.noreply.github.com>
2024-04-13 14:54:00 -0300

    feat: add an interface for validate all medical appointments

---
+++ TASK5/src/main/java/wedsan/task5/model/medicalAppointment/validators/MedicalAppointmentValidators.java
@@ -0,0 +1,7 @@
+package wedsan.task5.model.medicalAppointment.validators;
+
+import wedsan.task5.dto.request.MedicalAppointmentDTOReq;
+
+public interface MedicalAppointmentValidators {
+    void validate(MedicalAppointmentDTOReq req);
+}
