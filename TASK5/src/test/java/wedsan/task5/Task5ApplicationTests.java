1249e6adf17946639b202cd1197cbe80b9153055
André <120421201+WedSan@users.noreply.github.com>
2024-04-13 12:51:02 -0300

    feat: add default project files for task5

---
+++ TASK5/src/test/java/wedsan/task5/Task5ApplicationTests.java
@@ -0,0 +1,13 @@
+package wedsan.task5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
