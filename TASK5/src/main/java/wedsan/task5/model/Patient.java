e972c2fbfb1f5ec4abc549f98d57ac226d932542
André <120421201+WedSan@users.noreply.github.com>
2024-04-13 12:51:16 -0300

    feat: add Patient class to represent a patient table in DB

---
+++ TASK5/src/main/java/wedsan/task5/model/Patient.java
@@ -0,0 +1,81 @@
+package wedsan.task5.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TB_PATIENT")
+public class Patient {
+
+    @Id
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String phone;
+
+    private Address address;
+
+    private String document;
+
+    public Patient(){}
+
+    public Patient(Long id, String name, String email, String phone, String document) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.document = document;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+}
