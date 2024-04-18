7fbbc9cd0e45857b2e75fa74a630c6fae746aeea
André <120421201+WedSan@users.noreply.github.com>
2024-04-17 21:35:41 -0300

    feat: checks if the patient no longer has a scheduled appointment on the day

---
+++ TASK5/src/main/java/wedsan/task5/model/medicalAppointment/validators/PatientWithoutAnotherAppointmentValidator.java
@@ -0,0 +1,22 @@
+package wedsan.task5.model.medicalAppointment.validators;
+
+import wedsan.task5.dto.request.MedicalAppointmentDTOReq;
+import wedsan.task5.exception.ValidationException;
+import wedsan.task5.repository.MedicalAppointmentRepository;
+
+import java.time.LocalDateTime;
+
+public class PatientWithoutAnotherAppointmentValidator implements MedicalAppointmentValidators {
+
+    private MedicalAppointmentRepository repository;
+
+    @Override
+    public void validate(MedicalAppointmentDTOReq req) {
+        LocalDateTime firstTime = req.date().withHour(7);
+        LocalDateTime lastTime = req.date().withHour(18);
+        boolean existsAppointmentOnTheDay = repository.existsByPacienteIdAndDataBetween(req.idPatient(), firstTime, lastTime);
+        if (existsAppointmentOnTheDay) {
+            throw new ValidationException("The patient already has an appointment scheduled that day");
+        }
+    }
+}
--- TASK5/src/main/java/wedsan/task5/repository/MedicalAppointmentRepository.java
+++ TASK5/src/main/java/wedsan/task5/repository/MedicalAppointmentRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import wedsan.task5.model.medicalAppointment.MedicalAppointment;
 
+import java.time.LocalDateTime;
+
 @Repository
 public interface MedicalAppointmentRepository extends JpaRepository<MedicalAppointment, Long> {
+    boolean existsByPacienteIdAndDataBetween(Long idPaciente, LocalDateTime primeiroHorario,
+                                             LocalDateTime ultimoHorario);
 }
