374316466d71bede993a3b515c8237413d72f4a4
André <120421201+WedSan@users.noreply.github.com>
2024-04-18 21:28:13 -0300

    refactor: Extract error message creation into a separate method

--- TASK5/src/main/java/wedsan/task5/exception/GlobalExeptionHandler.java
+++ TASK5/src/main/java/wedsan/task5/exception/GlobalExeptionHandler.java
@@ -7,24 +7,29 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import wedsan.task5.dto.response.BasicErrorMessage;
+import wedsan.task5.dto.response.RequestErrorResponse;
 
 @RestControllerAdvice
 public class GlobalExeptionHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity handleError404(EntityNotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(createBasicMessageError(e.getMessage()));
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity handleError400(HttpMessageNotReadableException ex) {
-        return ResponseEntity.badRequest().body(ex.getMessage());
+        return ResponseEntity.badRequest().body(createBasicMessageError(ex.getMessage()));
     }
 
     @ExceptionHandler(ValidationException.class)
     public ResponseEntity handleBusinessRole(ValidationException ex) {
-        BasicErrorMessage basicErrorMessage = new BasicErrorMessage(ex.getMessage());
-        return ResponseEntity.badRequest().body(basicErrorMessage);
+        return ResponseEntity.badRequest().body(createBasicMessageError(ex.getMessage()));
+    }
+
+    private RequestErrorResponse createBasicMessageError(String errorMessage) {
+        BasicErrorMessage eMessage = new BasicErrorMessage(errorMessage);
+        return new RequestErrorResponse(eMessage);
     }
 
 }
